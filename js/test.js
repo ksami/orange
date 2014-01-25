ae3fd96cb60a048813a2efb70943a78a3604b427
Kenneth Ang <ksami.ihide@gmail.com>
2014-01-25 19:44:58 +0800

    more colours

--- js/test.js
+++ js/test.js
@@ -4,17 +4,26 @@ function helloWorld() {
   $("#word1").html('helloWorld!');
 }
 
-//todo more colours
 function brandColour() {
   var myColours = new Array();
   myColours[0] = "#03c03c";
   myColours[1] = "#ff0000";
   myColours[2] = "#0000ff";
   myColours[3] = "#ff6600";
+  myColours[4] = "#cfafe8";
+  myColours[5] = "#f0db3e";
+  myColours[6] = "#ff4980";
+  myColours[7] = "#3e6bf0";
+  myColours[8] = "#8ef03e";
+  myColours[9] = "#f0533e";
 
-  var i = Math.floor(Math.random()*4);
+  var i = Math.floor(Math.random()*10);
 
   $("#brand:hover").css("color", myColours[i]);
+}
+
+function wordEntry() {
+  
 }
 
 // $(function () {
