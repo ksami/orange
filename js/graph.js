adccbc2d7ec2a798b0086a838aa782922910f69b
Zjion <chngguohao@hotmail.com>
2014-01-26 11:20:47 +0800

    i give you example. the graph.js is screwed up btw.

--- js/graph.js
+++ js/graph.js
@@ -1,14 +1,28 @@
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart);
+//IN PROGRESS:::run interval after timer starts and stop interval mining after time stop
+
+var data = google.visualization.DataTable();
+data.addColumn('string', 'Time Lapsed');
+data.addcolumn('number', 'Alright');
+data.addcolumn('number', 'This');
 
 setInterval(mineData(),10000);
 var tempData;
-function minData() {
-  var tempTime = document.getElementById('startstopclock').clock.getTime().getSeconds();
-  var tempRate = count1 / 
-
+function mineData() {
+  var tempsecond = document.getElementById('startstopclock').clock.getTime().getSeconds();
+  var tempminute = document.getElementById('startstopclock').clock.getTime().getMinutes();
+  var temphour = document.getElementById('startstopclock').clock.getTime().getHours();
+  var temptimelapsed = tempsecond + tempminute*60 + temphour*3600;
+  if (temptimelapsed != 0)
+  {
+     var dataPoint = new Array();
+     dataPoint[0] = temptimelapsed.toString();
+     dataPoint[1] = count1;
+     dataPoint[2] = count2;
+     data.addRows(dataPoint);
+  }
 }
-var data = google.visualization.DataTable();
   ['Time', 'Alright', 'This'],
   ['0:30',         2,      3],
   ['1:00',         3,      3],
